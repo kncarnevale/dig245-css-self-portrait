b1d361c1cf6bfbff3e1f9f3ec5cfd4d1eb5065b0
Kate Carnevale <kacarnevale@davidson.edu>
2022-09-14 10:43:33 -0400

    organized code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,30 @@
 
 /* javascript */
 
-var bauhaus = `
+// var bauhaus = `
+//
+// Staaatliches Bauhaus (1919-1933)
+//
+//       ..-""T-..
+//     ,'__   |   '.
+//    /   N|  |     !
+//   :     '  |      :
+//   [      __|      ]
+//   :      _I       :
+//    !   ___I      /
+//     '.  N]     ,'
+//       "-"!..--"
+//
+// `;
+//
+// console.log(bauhaus);
 
-Staaatliches Bauhaus (1919-1933)
+var num = document.querySelectorAll('div').length;
+console.log('Total Number of HTML Elements: ' +num);
 
-      ..-""T-..
-    ,'__   |   '.
-   /   N|  |     !
-  :     '  |      :
-  [      __|      ]
-  :      _I       :
-   !   ___I      /
-    '.  N]     ,'
-      "-"!..--"
-
-`;
-
-console.log(bauhaus);
+//HOW TO DO- when move mouse, get coordinates
+  document.addEventListener("mousemove", () => {
+    let mousex = event.clientX; // Gets Mouse X
+    let mousey = event.clientY; // Gets Mouse Y
+    console.log([mousex, mousey]); // Prints data
+  });
